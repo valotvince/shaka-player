6a4cb4fca420359cdb9c73f69ccd1412652ad820
Vincent Valot <valot.vince@gmail.com>
2020-07-31 15:32:13 +0200

    prevent null access, re-order for cast priority, fixup some tests

--- test/media/drm_engine_unit.js
+++ test/media/drm_engine_unit.js
@@ -475,7 +475,7 @@ describe('DrmEngine', () => {
           .toHaveBeenCalledWith('drm.def', [jasmine.objectContaining({
             distinctiveIdentifier: 'optional',
             persistentState: 'required',
-            sessionTypes: ['temporary'],
+            sessionTypes: ['persistent-license'],
           })]);
     });
 
@@ -1812,6 +1812,7 @@ describe('DrmEngine', () => {
       // Key IDs in manifest
       tweakDrmInfos((drmInfos) => {
         drmInfos[0].keyIds = new Set(['deadbeefdeadbeefdeadbeefdeadbeef']);
+        drmInfos[0].initData = [{initDataType: 'niceInitDataType'}];
       });
 
       config.advanced['drm.abc'] = {
@@ -1836,7 +1837,8 @@ describe('DrmEngine', () => {
         audioRobustness: 'good',
         videoRobustness: 'really_really_ridiculously_good',
         serverCertificate: undefined,
-        initData: [],
+        initData: [{initDataType: 'niceInitDataType'}],
+        initDataTypes: ['niceInitDataType'],
         keyIds: new Set(['deadbeefdeadbeefdeadbeefdeadbeef']),
       });
     });
