4df56c597dd639aca060354cace71bc7735262d1
Vincent Valot <valot.vince@gmail.com>
2020-08-03 17:07:13 +0200

    remove useless addings

--- test/media/drm_engine_unit.js
+++ test/media/drm_engine_unit.js
@@ -1891,7 +1891,6 @@ describe('DrmEngine', () => {
       // Key IDs in manifest
       tweakDrmInfos((drmInfos) => {
         drmInfos[0].keyIds = new Set(['deadbeefdeadbeefdeadbeefdeadbeef']);
-        drmInfos[0].initData = [{initDataType: 'niceInitDataType'}];
       });
 
       config.advanced['drm.abc'] = {
@@ -1916,7 +1915,6 @@ describe('DrmEngine', () => {
         audioRobustness: 'good',
         videoRobustness: 'really_really_ridiculously_good',
         serverCertificate: undefined,
-        initData: [{initDataType: 'niceInitDataType'}],
         keyIds: new Set(['deadbeefdeadbeefdeadbeefdeadbeef']),
       });
     });
