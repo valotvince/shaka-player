ad4ea7b87426180a7a64b1313f1cf36f662b7c5a
Vincent Valot <valot.vince@gmail.com>
2020-08-10 16:58:00 +0200

    fix wrongly moved line

--- test/media/drm_engine_unit.js
+++ test/media/drm_engine_unit.js
@@ -1931,8 +1931,8 @@ describe('DrmEngine', () => {
         audioRobustness: 'good',
         videoRobustness: 'really_really_ridiculously_good',
         serverCertificate: undefined,
-        keyIds: new Set(['deadbeefdeadbeefdeadbeefdeadbeef']),
         initData: [],
+        keyIds: new Set(['deadbeefdeadbeefdeadbeefdeadbeef']),
       });
     });
   });  // describe('getDrmInfo')
