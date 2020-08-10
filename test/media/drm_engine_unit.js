6b791f386ddb6bdb59a85731bc954ed6df2c4a8a
Vincent Valot <valot.vince@gmail.com>
2020-08-10 16:51:40 +0200

    fixup unit tests

--- test/media/drm_engine_unit.js
+++ test/media/drm_engine_unit.js
@@ -1935,6 +1935,7 @@ describe('DrmEngine', () => {
         videoRobustness: 'really_really_ridiculously_good',
         serverCertificate: undefined,
         keyIds: new Set(['deadbeefdeadbeefdeadbeefdeadbeef']),
+        initData: [],
       });
     });
   });  // describe('getDrmInfo')
