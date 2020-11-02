8d6623d59a91c5ee80995e55f6d491874983610a
Vincent Valot <valot.vince@gmail.com>
2020-11-02 17:38:50 +0100

    fixup unit tests

--- test/media/drm_engine_unit.js
+++ test/media/drm_engine_unit.js
@@ -463,7 +463,7 @@ describe('DrmEngine', () => {
           .toHaveBeenCalledWith('drm.def', [jasmine.objectContaining({
             distinctiveIdentifier: 'optional',
             persistentState: 'required',
-            sessionTypes: ['persistent-license'],
+            sessionTypes: ['temporary'],
           })]);
     });
 
