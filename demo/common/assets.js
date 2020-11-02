f3ae25de22bb42945cc74400412e73d536ef9733
Vincent Valot <valot.vince@gmail.com>
2020-11-02 15:40:14 +0100

    review

--- demo/common/assets.js
+++ demo/common/assets.js
@@ -966,7 +966,7 @@ shakaAssets.testAssets = [
       .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
       .addLicenseServer('com.microsoft.playready', 'https://content.uplynk.com/pr')
       .addLicenseServer('com.widevine.alpha', 'https://content.uplynk.com/wv')
-      .setPersistentStateRequired('com.microsoft.playready', true)
+      .setExtraConfig({drm: {advanced: {'com.microsoft.playready': {persistentStateRequired: true}}}})
       .setRequestFilter(shakaAssets.UplynkRequestFilter)
       .setResponseFilter(shakaAssets.UplynkResponseFilter),
   // Reliable Playready playback requires Edge 16+
@@ -984,7 +984,7 @@ shakaAssets.testAssets = [
       .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
       .addLicenseServer('com.microsoft.playready', 'https://content.uplynk.com/pr')
       .addLicenseServer('com.widevine.alpha', 'https://content.uplynk.com/wv')
-      .setPersistentStateRequired('com.microsoft.playready', true)
+      .setExtraConfig({drm: {advanced: {'com.microsoft.playready': {persistentStateRequired: true}}}})
       .setRequestFilter(shakaAssets.UplynkRequestFilter)
       .setResponseFilter(shakaAssets.UplynkResponseFilter),
   new ShakaDemoAssetInfo(
